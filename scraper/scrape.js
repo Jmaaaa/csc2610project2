720c5e8b5b768140afb74ba6e986549cf75a91d8
rileyzzz <rileylemmler@gmail.com>
2024-12-02 20:29:50 -0600

    update scraper again

--- scraper/scrape.js
+++ scraper/scrape.js
@@ -15,8 +15,8 @@ function parseLot(sectionName, dayName, data) {
     const td = data.querySelectorAll('td');
 
     // Hack to replace double spaces with single.
-    const lotName = td[0].textContent.trim().replace(/\s\s+/g, ' ');
-    const lotNumber = td[1].textContent.trim().replace(/\s\s+/g, ' ');
+    const lotName = td[0].textContent.trim().replace(/\s\s+/g, ' ').replace('St.', 'Street');
+    const lotNumber = td[1].textContent.trim().replace(/\s\s+/g, ' ').replace('St.', 'Street');
     const totalSpaces = parseInt(td[2].textContent);
     const percentFull = Array.from(td).slice(3).map(x => parseInt(x.textContent));
 
